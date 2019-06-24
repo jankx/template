bb37d6fce4b8651b950130208d908b7d5e37377b
Puleeno Nguyen <puleeno@gmail.com>
2019-06-24 07:16:46 +0700

    added hook

--- src/Traits/PageTemplate.php
+++ src/Traits/PageTemplate.php
@@ -6,20 +6,25 @@ trait PageTemplate
     public function index()
     {
         $this->getHeader();
+        do_action('before_home_content');
         $this->getContent(array('home', 'index'));
+        do_action('after_home_content');
         $this->getFooter();
     }
 
     public function page()
     {
         $this->getHeader();
+        do_action('before_page_content');
         $this->getContent('page');
+        do_action('after_page_content');
         $this->getFooter();
     }
 
     public function single()
     {
         $this->getHeader();
+        do_action('before_single_content');
         $this->getContent(array(
             sprintf(
                 'single/%s',
@@ -27,24 +32,29 @@ trait PageTemplate
             ),
             'single'
         ));
+        do_action('after_single_content');
         $this->getFooter();
     }
 
     public function archive()
     {
         $this->getHeader();
+        do_action('after_archive_content');
         $this->getContent(array(
             'archive'
         ));
+        do_action('after_archive_content');
         $this->getFooter();
     }
 
     public function search()
     {
         $this->getHeader();
+        do_action('after_search_content');
         $this->getContent(array(
             'search'
         ));
+        do_action('after_search_content');
         $this->getFooter();
     }
 }
