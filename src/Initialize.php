2cbe095374c7495a5613d169a4fd3fb3ea5fab27
Puleeno Nguyen <puleeno@gmail.com>
2019-07-09 07:04:38 +0700

    Support page template don't call jankx_page() function

--- src/Initialize.php
+++ src/Initialize.php
@@ -6,7 +6,11 @@ class Initialize
     public static function loadTemplateFunctions()
     {
         self::loadHelpers();
-        add_filter('template_include', array(__CLASS__, 'supportNotJankxTemplate'));
+
+        /**
+         * Detect template include to ensure Jankx framework is loaded
+         */
+        add_filter('template_include', array(__CLASS__, 'supportNotJankxTemplate'), 35);
     }
 
     public static function loadHelpers()
@@ -20,9 +24,35 @@ class Initialize
     {
         $relativePath = str_replace(WP_CONTENT_DIR, '', $templateFile);
         if (!preg_match('/^\/themes/', $relativePath)) {
+            self::addJankxHookViaWordPressNative();
             jankx_page($templateFile, true);
         } else {
             return $templateFile;
         }
+    }
+
+
+    public function addJankxHookViaWordPressNative()
+    {
+        add_action('get_header', function () {
+            do_action('jankx_before_get_header');
+        }, 5);
+        add_action('jankx_template_header', function () {
+            do_action('jankx_after_get_header');
+        }, 15);
+
+        add_action('jankx_template_header', function () {
+            do_action('jankx_before_main_content');
+        }, 35);
+        add_action('get_footer', function () {
+            do_action('jankx_after_main_content');
+        }, 5);
+
+        add_action('get_footer', function () {
+            do_action('jankx_before_get_footer');
+        }, 35);
+        add_action('jankx_template_footer', function () {
+            do_action('jankx_after_get_footer');
+        });
     }
 }
