51911f228cf38cdc56d2d96d0b183632d5c818f7
Puleeno Nguyen <puleeno@gmail.com>
2020-01-04 16:01:18 +0700

    Fix 404 layout on mobile

--- default/404.php
+++ default/404.php
@@ -251,6 +251,20 @@ body, html {
 	color: #0b334f;
 	margin-top: 20px;
 }
+@media(max-width: 767px) {
+	.content {
+		padding: 20px;
+		position: relative;
+		top: 300px;
+	}
+
+	#yetiSVG {
+		width: 100%; height: 100%;
+	}
+	#lightSVG {
+		width: 100%; height: 100%;
+	}
+}
 </style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
 <script>
@@ -271,20 +285,20 @@ body, html {
 <script>
 var
 	yetiTL, chatterTL,
-    	furLightColor = "#FFF",
-    	furDarkColor = "#67b1e0",
-    	skinLightColor = "#ddf1fa",
-    	skinDarkColor = "#88c9f2",
-    	lettersSideLight = "#3A7199",
-    	lettersSideDark = "#051d2c",
-    	lettersFrontLight = "#67B1E0",
-    	lettersFrontDark = "#051d2c",
-    	lettersStrokeLight = "#265D85",
-    	lettersStrokeDark = "#031219",
-    	mouthShape1 = "M149 115.7c-4.6 3.7-6.6 9.8-5 15.6.1.5.3 1.1.5 1.6.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.4-1-.7-1.5-2.8-5.2-8.4-8.3-14.1-7.9-3.7.2-5.9 1.1-8.7 3.2z",
-    	mouthShape2 = "M161.2 118.9c0 2.2-1.8 4-4 4s-4-1.8-4-4c0-1 .4-2 1.1-2.7.7-.8 1.8-1.3 2.9-1.3 2.2 0 4 1.7 4 4z",
-    	mouthShape3 = "M150.2 118.3c-4.6 3.7-7.5 6.4-6.3 12.3.1.5.1.6.3 1.1.6 1.5 2.4 2.3 3.9 1.7 0 0 7.9-4.3 10.7-5.5s11.6-3.3 11.6-3.3c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.2-.6-.4-1.1-2.8-5.2-7.1-4.9-12.9-4.6-3.7.4-6.3 1.5-8.6 3.3z",
-    	mouthShape4 = "M149.2 116.7c-4.6 3.7-6.7 8.8-5.2 14.6.1.3.1.5.2.8.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.1-.3-.2-.5-.4-.7-2.8-5.2-8.2-7.2-14-6.9-3.6.2-5.9 1.1-8.6 3.2z"
+		furLightColor = "#FFF",
+		furDarkColor = "#67b1e0",
+		skinLightColor = "#ddf1fa",
+		skinDarkColor = "#88c9f2",
+		lettersSideLight = "#3A7199",
+		lettersSideDark = "#051d2c",
+		lettersFrontLight = "#67B1E0",
+		lettersFrontDark = "#051d2c",
+		lettersStrokeLight = "#265D85",
+		lettersStrokeDark = "#031219",
+		mouthShape1 = "M149 115.7c-4.6 3.7-6.6 9.8-5 15.6.1.5.3 1.1.5 1.6.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.4-1-.7-1.5-2.8-5.2-8.4-8.3-14.1-7.9-3.7.2-5.9 1.1-8.7 3.2z",
+		mouthShape2 = "M161.2 118.9c0 2.2-1.8 4-4 4s-4-1.8-4-4c0-1 .4-2 1.1-2.7.7-.8 1.8-1.3 2.9-1.3 2.2 0 4 1.7 4 4z",
+		mouthShape3 = "M150.2 118.3c-4.6 3.7-7.5 6.4-6.3 12.3.1.5.1.6.3 1.1.6 1.5 2.4 2.3 3.9 1.7 0 0 7.9-4.3 10.7-5.5s11.6-3.3 11.6-3.3c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.2-.6-.4-1.1-2.8-5.2-7.1-4.9-12.9-4.6-3.7.4-6.3 1.5-8.6 3.3z",
+		mouthShape4 = "M149.2 116.7c-4.6 3.7-6.7 8.8-5.2 14.6.1.3.1.5.2.8.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.1-.3-.2-.5-.4-.7-2.8-5.2-8.2-7.2-14-6.9-3.6.2-5.9 1.1-8.6 3.2z"
 ;
 
 chatterTL = new TimelineMax({paused: true, repeat: -1, yoyo: true});
