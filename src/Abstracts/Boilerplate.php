35b61600672cd19529753f7f135587b347faf9e5
Puleeno Nguyen <puleeno@gmail.com>
2020-02-23 18:02:30 +0700

    Remove old structure

--- src/Abstracts/Boilerplate.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Jankx\Template\Abstracts;
-
-use Jankx\Template\Interfaces\Boilerplate as InterfaceBoilerplate;
-
-abstract class Boilerplate implements InterfaceBoilerplate
-{
-}
--- src/UI/Breadcrumb.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Jankx\Template\UI;
-
-class Breadcrumb
-{
-
-}
