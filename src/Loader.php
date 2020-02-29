2184538469286b93747134a43673602973e970e6
Puleeno Nguyen <puleeno@gmail.com>
2020-02-29 23:04:32 +0700

    Follow template engine via template

--- src/Loader.php
+++ src/Loader.php
@@ -1,14 +1,33 @@
 <?php
 namespace Jankx\Template;
 
+use Jankx\Template\Engine\Engine;
+
 class Loader
 {
+    protected $templateEngine;
     protected $defaultTemplateDirectory;
+    protected $directoryInTheme;
     protected $searchedTemplate;
 
-    public function __construct($defaultTemplateDirectory, $themePrefix = '')
+    public function __construct($defaultTemplateDirectory, $directoryInTheme = '', $engine = null)
     {
         $this->defaultTemplateDirectory = $defaultTemplateDirectory;
+        $this->directoryInTheme = $directoryInTheme;
+
+        if (!is_null($engine)) {
+            $this->setTemplateEngine($engine);
+        }
+    }
+
+    public function setTemplateEngine($engine)
+    {
+        if (!($engine instanceof Engine)) {
+            throw new \Error(
+                sprintf('The template engine must is an instance of %s', Engine::class)
+            );
+        }
+        $this->templateEngine = $engine;
     }
 
     public function search($templates, $context = null)
--- src/Template.php
+++ src/Template.php
@@ -1,23 +1,61 @@
 <?php
 namespace Jankx\Template;
 
+use Jankx\Template\Engine\EngineManager;
+
 class Template
 {
     protected static $instances = [];
+    protected static $templateLoaded;
 
-    public static function getInstance($templateFileLoader = null, $themePrefix = '')
+    public static function getInstance($templateFileLoader = null, $themePrefix = '', $engineName = 'wordpress')
     {
         if (empty(self::$instances[$templateFileLoader])) {
-            self::$instances[$templateFileLoader] = new Loader($templateFileLoader, $themePrefix);
+            $loader = new Loader(
+                $templateFileLoader,
+                $themePrefix
+            );
+            $applyTemplateEngine = apply_filters(
+                'jankx_template_engine',
+                $engineName,
+                $templateFileLoader,
+                $themePrefix,
+                $loader
+            );
+            $engine = EngineManager::createEngine(
+                $templateFileLoader,
+                $applyTemplateEngine,
+                array(
+                    'template_location' => $templateFileLoader,
+                )
+            );
+            $loader->setTemplateEngine($engine);
+
+            self::$instances[$templateFileLoader] = $loader;
         }
         return self::$instances[$templateFileLoader];
     }
 
-    public static function getTemplateEngine()
-    {
-    }
-
     public function load()
     {
+        if (self::$templateLoaded) {
+            return;
+        }
+        $this->loadTemplateHelpers();
+
+        /**
+         * Create a flag to check Jankx template library is loaded
+         */
+        self::$templateLoaded = true;
+    }
+
+    public function loadTemplateHelpers()
+    {
+        $helpersDir = realpath(dirname(__FILE__) . '/../helpers');
+
+        /**
+         * Load Jankx template helpers
+         */
+        require_once $helpersDir . '/template.php';
     }
 }
