09d6d3e2741161c3da5e62818c7ee34d895c2d9f
Puleeno Nguyen <puleeno@gmail.com>
2020-09-06 18:43:38 +0700

    Add title to template

--- default/header.php
+++ default/header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<link rel="profile" href="https://gmpg.org/xfn/11">
+    <title><?php wp_title(); ?></title>
 		<?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
