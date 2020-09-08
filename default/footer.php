f12e0e9c2fe8432abf18247e8c80df88010c41f9
Puleeno Nguyen <puleeno@gmail.com>
2020-09-08 18:47:07 +0700

    Fix class name

--- default/footer.php
+++ default/footer.php
@@ -1,11 +1,15 @@
   <?php do_action( 'jankx_template_before_footer' ); ?>
-    <footer id="jankx-site-footer" class="jankx-site-header">
+
+    <footer id="jankx-site-footer" class="jankx-site-footer">
+
       <?php do_action('jankx_template_before_footer_content'); ?>
 
         <?php jankx_component( 'footer' ); ?>
 
       <?php do_action('jankx_template_after_footer_content'); ?>
+
     </footer>
+
   <?php do_action( 'jankx_template_after_footer' ); ?>
 
   <?php wp_footer(); ?>
