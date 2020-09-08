29c9741f09dbba7efd85f7c373715213e393947e
Puleeno Nguyen <puleeno@gmail.com>
2020-09-08 22:25:08 +0700

    Add footer widget templates

---
+++ default/footer/open-widget-areas.php
@@ -0,0 +1,2 @@
+<div class="footer-widgets-area <?php echo $footer_widget_classes; ?>">
+    <div class="widgets-area-inner">
--- default/sidebar/alt.php
+++ default/sidebar/alt.php
@@ -4,7 +4,7 @@
         dynamic_sidebar('secondary');
     } elseif(current_user_can('edit_theme_options')) {
         printf(
-            __('Please add the widgets to this sidebar at <a href="%s">Widget Dashboard</a>', 'jankx'),
+            __('Please add the widgets to this sidebar at <a href="%s">Widget Dashboard</a>. Only you see this message because you are the moderator.', 'jankx'),
             admin_url('widgets.php')
         );
     }
