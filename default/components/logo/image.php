9169ce5aaa4caaf52d2e1b564ffd45531a990fbf
Puleeno Nguyen <puleeno@gmail.com>
2020-09-09 15:59:18 +0700

    Create new template for logo component

---
+++ default/components/logo/image.php
@@ -0,0 +1,12 @@
+<div class="jankx-logo image">
+    <?php printf('<%s class="logo-text-wrap">', $wrap_tag); ?>
+        <?php if($url): ?>
+            <a href="<?php echo $url; ?>" style="background: url(<?php echo $image_url; ?>)">
+                <?php echo $text; ?>
+            </a>
+        <?php else: ?>
+            <span><?php echo $text; ?></span>
+            <img src="<?php echo $image_url; ?>" alt="<?php echo $text; ?>">
+        <?php endif; ?>
+    <?php printf('</%s>', $wrap_tag); ?>
+</div>
