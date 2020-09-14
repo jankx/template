8490b0f2b8d7dd1801425f9ecec2d54e2e6befbe
Puleeno Nguyen <puleeno@gmail.com>
2020-09-14 09:50:15 +0700

    Add link to thumbnail

--- default/post/loop/large-image.php
+++ default/post/loop/large-image.php
@@ -1,5 +1,9 @@
 <div class="<?php post_class(); ?>">
-    <div class="post-thumbnail"><?php jankx_the_post_thumbnail('medium'); ?></div>
+    <div class="post-thumbnail">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php jankx_the_post_thumbnail('medium'); ?>
+        </a>
+    </div>
     <div class="post-infos">
         <h2 class="post-title">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
--- default/post/loop/list-item.php
+++ default/post/loop/list-item.php
@@ -1,5 +1,7 @@
 <div class="<?php post_class(); ?>">
-    <div class="post-thumbnail"><?php jankx_the_post_thumbnail('thumbnail'); ?></div>
+    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <?php jankx_the_post_thumbnail('thumbnail'); ?>
+    </a>
     <div class="post-infos">
         <h2 class="post-title">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
