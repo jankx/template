14be2a836c3abab2488382b6a93be3ebab4ca8ae
Puleeno Nguyen <puleeno@gmail.com>
2020-09-14 21:01:19 +0700

    update single template

--- default/content/single.php
+++ default/content/single.php
@@ -1,3 +1,6 @@
-<?php
-
-the_content();
+<article <?php post_class(); ?>>
+    <h1 class="jankx-header page-header"><?php the_title(); ?></h1>
+    <div class="post-content">
+        <?php the_content(); ?>
+    </div>
+</article>
