9b232809a5f2720d582a98b23ae30e54111f0d02
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 04:47:30 +0700

    Fix logo image background

--- default/components/logo/image.php
+++ default/components/logo/image.php
@@ -1,7 +1,7 @@
 <div class="jankx-logo image">
     <?php printf('<%s class="logo-text-wrap">', $wrap_tag); ?>
         <?php if ($url) : ?>
-            <a href="<?php echo $url; ?>" style="background: url(<?php echo $image_url; ?>)">
+            <a href="<?php echo $url; ?>" style="background-image: url(<?php echo $image_url; ?>)">
                 <?php echo $text; ?>
             </a>
         <?php else : ?>
