91acdb6ed8c028d96fe0af7ef9ba5125f11e9c45
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 21:32:09 +0700

    Delete meta viewport to prepare create desktop version on mobile device

--- default/header.php
+++ default/header.php
@@ -2,7 +2,6 @@
 <html class="no-js" <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
         <link rel="profile" href="https://gmpg.org/xfn/11">
         <title><?php wp_title(); ?></title>
         <?php wp_head(); ?>
