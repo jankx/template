f7503458d2f2de08939f261a6548fe94c0379fe3
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 13:47:51 +0700

    Support container for navigation

--- default/components/navigation.php
+++ default/components/navigation.php
@@ -1,5 +1,14 @@
 <nav class="jankx-ui navigation navigation-<?php echo $args['theme_location']; ?>">
+
     <?php
+        if ($args['open_container']) {
+            jankx_open_container();
+        }
+
         wp_nav_menu($args);
+
+        if ($args['open_container']) {
+            jankx_close_container();
+        }
     ?>
 </nav>
