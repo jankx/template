ae34f2f05c90c8ace3960d16e4a9588f769b254a
Puleeno Nguyen <puleeno@gmail.com>
2020-10-05 10:05:22 +0700

    Move footer content to other template file

---
+++ default/footer-content.php
@@ -0,0 +1,27 @@
+<?php do_action('jankx_template_before_footer'); ?>
+
+    <footer id="jankx-site-footer" class="jankx-site-footer">
+
+        <?php do_action('jankx_template_before_footer_content'); ?>
+
+            <?php
+                jankx_component(
+                    'footer',
+                    array(
+                        'copyright' => sprintf(
+                            __('Copyright &copy; %1$d <a href="%2$s">%3$s</a>.', 'jankx'),
+                            date('Y'),
+                            site_url(),
+                            get_bloginfo('name')
+                        ),
+                    ),
+                    array(
+                        'echo' => true
+                    )
+                ); ?>
+
+        <?php do_action('jankx_template_after_footer_content'); ?>
+
+    </footer>
+
+<?php do_action('jankx_template_after_footer'); ?>
--- default/footer.php
+++ default/footer.php
@@ -1,30 +1,6 @@
-    <?php do_action('jankx_template_before_footer'); ?>
-
-        <footer id="jankx-site-footer" class="jankx-site-footer">
-
-            <?php do_action('jankx_template_before_footer_content'); ?>
-
-                <?php
-                    jankx_component(
-                        'footer',
-                        array(
-                            'copyright' => sprintf(
-                                __('Copyright &copy; %1$d <a href="%2$s">%3$s</a>.', 'jankx'),
-                                date('Y'),
-                                site_url(),
-                                get_bloginfo('name')
-                            ),
-                        ),
-                        array(
-                            'echo' => true
-                        )
-                    ); ?>
-
-            <?php do_action('jankx_template_after_footer_content'); ?>
-
-        </footer>
-
-    <?php do_action('jankx_template_after_footer'); ?>
+    <?php if (jankx_template_has_footer()): ?>
+        <?php jankx_template('footer-content'); ?>
+    <?php endif; ?>
 
     <?php wp_footer(); ?>
     </body>
