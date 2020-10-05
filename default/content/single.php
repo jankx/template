7ad18c6de01a7fa18c3da29a6d1b67e5aa321378
Puleeno Nguyen <puleeno@gmail.com>
2020-10-05 08:28:40 +0700

    Support hide single page header

--- default/content/single.php
+++ default/content/single.php
@@ -1,5 +1,7 @@
 <article <?php post_class(); ?>>
+    <?php if (apply_filters('jankx_template_page_single_page_header', true, $GLOBALS['post'])): ?>
     <h1 class="jankx-header page-header"><?php the_title(); ?></h1>
+    <?php endif; ?>
     <div class="post-content">
         <?php the_content(); ?>
     </div>
