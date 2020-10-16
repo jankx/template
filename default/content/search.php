69e246990340e5ae90c20cec6c072533034a9821
Puleeno Nguyen <puleeno@gmail.com>
2020-10-16 11:08:00 +0700

    Change search layout to card

--- default/content/search.php
+++ default/content/search.php
@@ -8,49 +8,28 @@ use Jankx\PostLayout\PostLayoutManager;
         <?php echo sprintf(__('Search results for "%s"', 'jankx'), get_search_query()); ?>
     </h1>
     <?php
-    global $wp_query, $post;
+    global $wp_query;
     if ($wp_query->have_posts()):
-    $grouped_posts = array();
-
-    foreach($wp_query->posts as $post) {
-        $grouped_posts[$post->post_type][] = $post;
-    }
-    asort($grouped_posts);
-    ?>
-
-    <?php foreach($grouped_posts as $post_type => $posts): ?>
-        <?php
-            $post_type_object = get_post_type_object($post_type);
         ?>
         <h2 class="search-subtitle"><?php echo $post_type_object->label; ?></h2>
         <div class="search-resuls-<?php echo $post_type; ?>">
         <?php
-            $clone_wp_query = clone $wp_query;
-            $clone_wp_query->posts = $posts;
-            $clone_wp_query->post_count = count($posts);
-            $customHook = "jankx_search_results_{$post_type}";
-            if (has_action($customHook)) {
-                do_action($customHook, $clone_wp_query);
-            } else {
-                $layoutManager = PostLayoutManager::getInstance();
-                $layoutStyle   = apply_filters(
-                    "jankx_search_{$post_type}_layout_style",
-                    PostLayoutManager::PRESET_1
-                );
-                $layoutCls     = $layoutManager->getLayoutClass($layoutStyle);
+            $layoutManager = PostLayoutManager::getInstance();
+            $layoutStyle   = apply_filters(
+                "jankx_search_results_layout_style",
+                PostLayoutManager::CARD
+            );
+            $layoutCls     = $layoutManager->getLayoutClass($layoutStyle);
 
-                // Create post layout style instance
-                $postLayoutInstance = new $layoutCls($clone_wp_query);
+            // Create post layout style instance
+            $postLayoutInstance = new $layoutCls($wp_query);
 
-                // Render posts
-                if (is_a($postLayoutInstance, PostLayout::class)) {
-                    echo $postLayoutInstance->render();
-                }
+            // Render posts
+            if (is_a($postLayoutInstance, PostLayout::class)) {
+                echo $postLayoutInstance->render();
             }
         ?>
         </div>
-    <?php endforeach; ?>
-
     <?php else: ?>
     <div class="no-results"><?php _e('Not found', 'jankx'); ?></div>
     <?php endif; ?>
