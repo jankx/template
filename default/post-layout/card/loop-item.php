55f8dda7a9fc7e95c2e5651a3f6219bfe36c7f62
Puleeno Nguyen <puleeno@gmail.com>
2020-10-21 02:58:27 +0700

    Fix jankx the post thumbnail

--- default/post-layout/card/loop-item.php
+++ default/post-layout/card/loop-item.php
@@ -2,7 +2,7 @@
     <?php if (!empty($show_thumbnail)) : ?>
     <div class="post-thumbnail">
         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php jankx_the_post_thumbnail('thumbnail'); ?>
+            <?php jankx_the_post_thumbnail($thumbnail_size); ?>
         </a>
     </div>
     <?php endif; ?>
