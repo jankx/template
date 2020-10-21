13efa100429af55d00d2ec073e6060c5ff22bcdd
Puleeno Nguyen <puleeno@gmail.com>
2020-10-21 20:53:52 +0700

    Fix image size

--- default/post-layout/loop-item.php
+++ default/post-layout/loop-item.php
@@ -1,7 +1,7 @@
 <div <?php post_class('loop-item'); ?>>
     <div class="post-thumbnail">
         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php jankx_the_post_thumbnail('thumbnail'); ?>
+            <?php jankx_the_post_thumbnail($thumbnail_size); ?>
         </a>
     </div>
     <div class="post-infos">
