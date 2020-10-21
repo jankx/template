dc9341f12a1aaf1b11ce557a351541f5b55eecd5
Puleeno Nguyen <puleeno@gmail.com>
2020-10-21 15:02:29 +0700

    support template show/hide post title

--- default/post-layout/loop-item.php
+++ default/post-layout/loop-item.php
@@ -5,8 +5,10 @@
         </a>
     </div>
     <div class="post-infos">
+        <?php if ($show_title): ?>
         <h2 class="post-title">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
         </h2>
+        <?php endif; ?>
     </div>
 </div>
