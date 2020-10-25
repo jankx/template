f0c43ba5f01548252850dbb309cf4e08624a140d
Puleeno Nguyen <puleeno@gmail.com>
2020-10-25 20:16:06 +0700

    Update footer component template

--- default/layout/footer/copyright.php
+++ default/layout/footer/copyright.php
@@ -1,6 +1,8 @@
-<?php jankx_open_container(); ?>
-    <div class="jankx-copyright">
-        <?php echo $copyright; ?>
-        <?php echo $jankx_credit; ?>
-    </div>
-<?php jankx_close_container(); ?>
+<div class="jankx-copyright">
+    <?php jankx_open_container(); ?>
+        <div class="text">
+            <?php echo $copyright; ?>
+            <?php echo $jankx_credit; ?>
+        </div>
+    <?php jankx_close_container(); ?>
+</div>
