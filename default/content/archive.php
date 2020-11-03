f9d01f20492839fb2b6c646dfe51cfab81ffea7e
Puleeno Nguyen <puleeno@gmail.com>
2020-11-03 15:05:05 +0700

    Support archive content

--- default/content/archive.php
+++ default/content/archive.php
@@ -2,6 +2,7 @@
 /**
  * Content page index
  */
+use Jankx\PostLayout\PostLayoutManager;
 
 /**
  * The index page content will be render via action hook
@@ -9,10 +10,25 @@
  * Hooked:
  *  - N/A
  */
-
+$template_hook = 'jankx_template_archive_content';
 if (is_post_type_archive()) {
-    do_action('jankx_template_page_post_type_archive_content', get_post_type());
+    $template_hook = 'jankx_template_page_post_type_archive_content';
 } elseif (is_tax()) {
-    $queried_object = get_queried_object();
-    do_action('jankx_template_page_taxonomy_content', $queried_object);
+    $template_hook = 'jankx_template_page_taxonomy_content';
+}
+
+if (has_action($template_hook)) {
+    do_action($template_hook, get_queried_object());
+} else {
+    $layoutManager = PostLayoutManager::getInstance();
+    $postLayoutCls = $layoutManager->getLayoutClass(PostLayoutManager::CARD);
+    $postLayout = new $postLayoutCls($GLOBALS['wp_query']);
+    $postLayout->setOptions(array(
+        'columns' => 4,
+        'show_excerpt' => true,
+        'post_meta_features' => array(
+            'post_date' => true,
+        ),
+    ));
+    echo $postLayout->render();
 }
