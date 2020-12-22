4e782f011a8b730d842ed8490cb2b6c56741eef6
Puleeno Nguyen <puleeno@gmail.com>
2020-12-22 17:45:36 +0700

    Added site content and site url to footer data

--- default/footer-content.php
+++ default/footer-content.php
@@ -5,15 +5,19 @@
         <?php do_action('jankx_template_before_footer_content'); ?>
 
             <?php
+                $site_name = get_bloginfo('name');
+                $site_url = site_url();
                 jankx_component(
                     'footer',
                     array(
                         'copyright' => sprintf(
                             __('Copyright &copy; %1$d <a href="%2$s">%3$s</a>.', 'jankx'),
                             date('Y'),
-                            site_url(),
-                            get_bloginfo('name')
+                            $site_url,
+                            $site_name
                         ),
+                        'site_name' => $site_name,
+                        'site_url' => $site_url,
                     ),
                     array(
                         'echo' => true
