a003f2bd2d3765431888e10b46860b83095de50e
Puleeno Nguyen <puleeno@gmail.com>
2020-12-31 22:42:32 +0700

    Delete sharing template

--- default/socials/sharing.php
+++
@@ -1,4 +0,0 @@
-<button id="ok" type="button">Share on Odnoklassniki</button>
-<button id="mail" type="button">Share on Mail.ru</button>
-<button id="email" type="button">Share via users's email</button>
-<button id="linkedin" type="button">Share on Linkedin</button>
