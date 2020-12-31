ab3f7d655eb0ac8acd540738576bc1f341e04b39
Puleeno Nguyen <puleeno@gmail.com>
2020-12-31 21:28:16 +0700

    Added default template sharing via socials

---
+++ default/socials/sharing.php
@@ -0,0 +1,4 @@
+<button id="ok" type="button">Share on Odnoklassniki</button>
+<button id="mail" type="button">Share on Mail.ru</button>
+<button id="email" type="button">Share via users's email</button>
+<button id="linkedin" type="button">Share on Linkedin</button>
