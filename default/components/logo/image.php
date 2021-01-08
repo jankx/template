61189c49c02a14fbe19f9986260a317b94173346
Puleeno Nguyen <puleeno@gmail.com>
2021-01-08 08:58:43 +0700

    Support logo inline styles

--- default/components/logo/image.php
+++ default/components/logo/image.php
@@ -1,7 +1,7 @@
 <div class="jankx-logo image <?php echo $class; ?>">
     <?php printf('<%s class="logo-text-wrap">', $wrap_tag); ?>
         <?php if ($url) : ?>
-            <a href="<?php echo $url; ?>" style="background-image: url(<?php echo $image_url; ?>)">
+            <a href="<?php echo $url; ?>" style="background-image: url(<?php echo $image_url; ?>)<?php echo $logo_size_styles; ?>">
                 <?php echo $text; ?>
             </a>
         <?php else : ?>
