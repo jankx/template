3cece671191df97ba683ac875ad4450a58b16871
Puleeno Nguyen <puleeno@gmail.com>
2021-03-21 16:00:29 +0700

    Create header.php

---
+++ default/components/header.php
@@ -0,0 +1,11 @@
+<header <?php echo jankx_generate_html_attributes($attributes); ?>>
+    <?php do_action('jankx_template_before_header_content'); ?>
+    <?php jankx_open_container(); ?>
+
+        <?php do_action('jankx_component_before_header'); ?>
+        <?php echo $content; ?>
+        <?php do_action('jankx_component_after_header'); ?>
+
+    <?php jankx_close_container(); ?>
+    <?php do_action('jankx_template_after_header_content'); ?>
+</header>
