6966b3a854b46bfbbb3b56044a5507bc0f38f63b
Puleeno Nguyen <puleeno@gmail.com>
2021-03-24 01:27:27 +0700

    Fix bug close site layout tags when page is setted do no t has footer content

--- default/footer.php
+++ default/footer.php
@@ -1,6 +1,8 @@
-    <?php if (jankx_template_has_footer()) : ?>
-        <?php jankx_template('footer-content'); ?>
-    <?php endif; ?>
+    <?php do_action('jankx_template_before_footer'); ?>
+        <?php if (jankx_template_has_footer()) : ?>
+            <?php jankx_template('footer-content'); ?>
+        <?php endif; ?>
+    <?php do_action('jankx_template_after_footer'); ?>
 
     <?php wp_footer(); ?>
     </body>
